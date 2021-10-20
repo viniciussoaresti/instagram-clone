51c92b282e5cb3a19e1b7018905e193da2f60261
Vinícius Soares <viniciussoaresti@gmail.com>
2021-10-20 14:54:28 -0300

    raw

--- main.js
+++
@@ -1,7 +0,0 @@
-function switchPages(element){
-    if(element.id == 'SwitchAccounts'){
-        window.open("https://www.instagram.com/_.vsti/");
-    } else{
-        window.open("https://www.instagram.com/accounts/emailsignup/");
-    }
-}
